4b6815a941a05d2972c45751dddce13c72e3c02a
Tal Glanzman <talglanzman@gmail.com>
2020-03-16 18:04:06 +0200

    nise

--- optimization/src/main/java/org/tglanz/limestone/optimization/OptimizationBootstrap.java
+++ optimization/src/main/java/org/tglanz/limestone/optimization/OptimizationBootstrap.java
@@ -3,13 +3,17 @@ package org.tglanz.limestone.optimization;
 import org.apache.calcite.plan.RelOptRule;
 import org.tglanz.limestone.ModuleBootstrap;
 import org.tglanz.limestone.optimization.rules.OptimizeAggregateToHaveLessCpuRule;
+import org.tglanz.limestone.optimization.rules.OptimizeAggregateToHaveMoreCpuRule;
 
 import java.util.Arrays;
 
 public class OptimizationBootstrap implements ModuleBootstrap {
     public Iterable<RelOptRule> getAllRules() {
         return Arrays.asList(
-                OptimizeAggregateToHaveLessCpuRule.Instance
+                // those two are an example! not what we really intend algorithmicly, but they show how the planner
+                // choose a correct implementation based on cost
+                OptimizeAggregateToHaveLessCpuRule.Instance,
+                OptimizeAggregateToHaveMoreCpuRule.Instance
         );
     };
 }
--- optimization/src/main/java/org/tglanz/limestone/optimization/rels/AggregateWithMoreCpuLimeRel.java
+++ optimization/src/main/java/org/tglanz/limestone/optimization/rels/AggregateWithMoreCpuLimeRel.java
@@ -6,24 +6,23 @@ import org.apache.calcite.plan.RelOptPlanner;
 import org.apache.calcite.plan.RelTraitSet;
 import org.apache.calcite.rel.RelNode;
 import org.apache.calcite.rel.core.Aggregate;
+import org.apache.calcite.rel.core.AggregateCall;
 import org.apache.calcite.rel.metadata.RelMetadataQuery;
 import org.apache.calcite.util.ImmutableBitSet;
-import org.apache.calcite.rel.core.AggregateCall;
-import org.tglanz.limestone.rels.AggregateLimeRel;
 import org.tglanz.limestone.rels.LimeRel;
 
 import java.util.List;
 
-public class AggregateWithLessCpuLimeRel
+public class AggregateWithMoreCpuLimeRel
         extends Aggregate
         implements LimeRel {
 
-    public AggregateWithLessCpuLimeRel(RelOptCluster cluster,
-                            RelTraitSet traitSet,
-                            RelNode input,
-                            ImmutableBitSet groupSet,
-                            List<ImmutableBitSet> groupSets,
-                            List<AggregateCall> aggCalls) {
+    public AggregateWithMoreCpuLimeRel(RelOptCluster cluster,
+                                       RelTraitSet traitSet,
+                                       RelNode input,
+                                       ImmutableBitSet groupSet,
+                                       List<ImmutableBitSet> groupSets,
+                                       List<AggregateCall> aggCalls) {
         super(cluster, traitSet, input, groupSet, groupSets, aggCalls);
         assert getConvention() == Convention;
     }
@@ -34,14 +33,14 @@ public class AggregateWithLessCpuLimeRel
                           ImmutableBitSet groupSet,
                           List<ImmutableBitSet> groupSets,
                           List<AggregateCall> aggCalls) {
-        return new AggregateWithLessCpuLimeRel(input.getCluster(), traitSet, input, groupSet, groupSets, aggCalls);
+        return new AggregateWithMoreCpuLimeRel(input.getCluster(), traitSet, input, groupSet, groupSets, aggCalls);
     }
 
     @Override
     public RelOptCost computeSelfCost(RelOptPlanner planner, RelMetadataQuery mq) {
         // RelOptCost cost = super.computeSelfCost(planner, mq);
         double rows = mq.getRowCount(this);
-        double cpu = 1 * rows; // lets say 1, for example add which will take 1 cycles
+        double cpu = 8 * rows; // lets say 1, for example add which will take 1 cycles
         double io = 2 * rows; // lets say 2, for example read, write
         return planner.getCostFactory().makeCost(rows, cpu, io);
     }
